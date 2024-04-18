fad8f6af9c16e681ba9456b0368c509ea6d7315d
Crystal Cheong <65748007+crystalcheong@users.noreply.github.com>
2024-04-18 17:36:45 +0800

    fix: corrected gitignore

---
+++ src/env.ts
@@ -0,0 +1,24 @@
+import { createEnv } from '@t3-oss/env-core'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(['development', 'test', 'production']),
+  },
+  clientPrefix: 'VITE_',
+  client: {
+    VITE_BETA_FLAG: z
+      .string()
+      /** @external https://env.t3.gg/docs/recipes#booleans */
+      .transform((s) => s !== 'false' && s !== '0'),
+
+  },
+  runtimeEnv: import.meta.env,
+
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+   * This is especially useful for Docker builds.
+   */
+  skipValidation: !!import.meta.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+})
